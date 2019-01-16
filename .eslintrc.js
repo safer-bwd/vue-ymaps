42c492fd0183b0544a972d6c914e11cd2d82d472
safer <asafron2@ford.com>
2019-01-16 14:34:55 +0300

    change eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,15 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
-  root: true,
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
+  "root": true,
+  "env": {
+    "node": true
   },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard',
-    // https://github.com/vuejs/eslint-plugin-vue
-    // 'plugin:vue/essential, `plugin:vue/strongly-recommended`, `plugin:vue/recommended`
-    'plugin:vue/essential',
+  "extends": [
+    "plugin:vue/essential",
+    "eslint:recommended"
   ],
-  // plugins: [
-  //   // required to lint *.vue file
-  //   `vue`
-  // ],
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  "rules": {},
+  "parserOptions": {
+    "parser": "babel-eslint"
   }
 }
