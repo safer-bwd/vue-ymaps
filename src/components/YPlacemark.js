db7cff0d0329dc640b715053f088e508e1f1b277
safer <asafron2@ford.com>
2019-09-20 10:45:03 +0300

    refactor: replace common code to GeoObject mixin

--- src/components/YPlacemark.js
+++ src/components/YPlacemark.js
@@ -1,17 +1,5 @@
 import GeoObject from '../mixins/GeoObject';
 
-// TODO add others events
-const boundEvents = [
-  'click'
-];
-
-// TODO add others props
-const boundProps = [
-  { name: 'coordinates', type: 'geometry' },
-  { name: 'options', type: 'options' },
-  { name: 'properties', type: 'properties' }
-];
-
 export default {
   name: 'y-placemark',
 
@@ -22,43 +10,10 @@ export default {
     coordinates: {
       type: Array,
       required: true
-    },
-
-    options: {
-      type: Object,
-      required: false,
-      default: () => ({})
-    },
-
-    properties: {
-      type: Object,
-      required: false,
-      default: () => ({})
     }
   },
 
-  async mounted () {
-    const ymaps = await this.readyYMapsApi();
-    const placemark = this.createPlacemark(ymaps);
-    this.setYMapsInstance(placemark);
-    this.bindEvents(boundEvents);
-    this.bindProps(boundProps);
-    await this.addToMap();
-    this.$emit('ready');
-  },
-
-  methods: {
-    createPlacemark (ymaps) {
-      const { Placemark } = ymaps;
-
-      const placemark = new Placemark(this.coordinates, {
-        ...this.properties
-      }, {
-        ...this.$attrs,
-        ...this.options
-      });
-
-      return placemark;
-    }
-  }
+  data: () => ({
+    type: 'Point'
+  })
 };
--- src/mixins/GeoObject.js
+++ src/mixins/GeoObject.js
@@ -1,29 +1,93 @@
 import MapElement from '../mixins/MapElement';
 
+// TODO add others events
+const boundEvents = [
+  'click'
+];
+
+// TODO add others props
+const boundProps = [
+  { name: 'coordinates', type: 'geometry' },
+  { name: 'options', type: 'options' },
+  { name: 'properties', type: 'properties' }
+];
+
 export default {
   mixins: [ MapElement ],
 
-  destroyed () {
-    this.removeFromMap();
+  props: {
+    coordinates: {
+      type: Array,
+      required: true
+    },
+
+    radius: {
+      type: Number,
+      required: false
+    },
+
+    options: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    },
+
+    properties: {
+      type: Object,
+      required: false,
+      default: () => ({})
+    }
+  },
+
+  data: () => ({
+    type: ''
+  }),
+
+  async mounted () {
+    const ymaps = await this.readyYMapsApi();
+    const geoObj = this.createGeoObject(ymaps);
+    this.setYMapsInstance(geoObj);
+    this.bindEvents(boundEvents);
+    this.bindProps(boundProps);
+    await this.addGeoObjectToMap(geoObj);
+    this.$emit('ready');
+  },
+
+  async destroyed () {
+    const geoObj = this.getYMapsInstance();
+    if (!geoObj) {
+      return;
+    }
+    await this.removeGeoObjectFromMap(geoObj);
   },
 
   methods: {
-    async addToMap () {
-      const obj = this.getYMapsInstance();
-      if (!obj) {
-        return;
-      }
-      const map = await this.getYMapsMap();
-      map.geoObjects.add(obj);
+    createGeoObject(ymaps) {
+      const { GeoObject } = ymaps;
+
+      const geoObj = new GeoObject({
+        geometry: {
+          type: this.type || 'Point',
+          coordinates: this.coordinates,
+          radius: this.radius
+        },
+        properties: { ... this.properties }
+      }, {
+        ...this.$attrs,
+        ...this.options
+      });
+
+      return geoObj;
     },
 
-    async removeFromMap () {
-      const obj = this.getYMapsInstance();
-      if (!obj) {
-        return;
-      }
+    async addGeoObjectToMap(geoObj) {
       const map = await this.getYMapsMap();
-      map.geoObjects.remove(obj);
+      map.geoObjects.add(geoObj);
+    },
+
+    async removeGeoObjectFromMap (geoObj) {
+      const map = await this.getYMapsMap();
+      map.geoObjects.remove(geoObj);
     }
   }
 };
